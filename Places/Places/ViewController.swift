5b7aaea8796104bfc0172d384584850fda20700f
Oleksandr Kachkin <oleksandr.severo@gmail.com>
2022-07-07 16:06:01 +0300

    Initial commit

---
+++ Places/Places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Oleksandr Kachkin on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
