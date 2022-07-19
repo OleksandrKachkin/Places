ee2606375ada5fb26d691f82da9c03ee46c1e3f7
Oleksandr Kachkin <oleksandr.severo@gmail.com>
2022-07-19 12:32:54 +0300

    test on feature/list branch

---
+++ Places/Places/Controllers/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  PlacesV2_TEST
+//
+//  Created by Oleksandr Kachkin on 14.07.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var iconOfPlace: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var addressLabel: UILabel!
+  @IBOutlet weak var ratingPLace: UILabel!
+}
