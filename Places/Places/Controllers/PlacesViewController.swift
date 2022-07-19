22a98fdb5d0af2fcd3e92ae3afac7ccb187f14ec
Oleksandr Kachkin <oleksandr.severo@gmail.com>
2022-07-19 14:04:08 +0300

    Add Feature №2

--- Places/Places/Controllers/PlacesViewController.swift
+++ Places/Places/Controllers/PlacesViewController.swift
@@ -8,22 +8,64 @@
 import UIKit
 
 class PlacesViewController: UIViewController {
+  
+  var networkManager = NetworkManager()
+  var placesArray = [GooglePlace]()
+  private let cellReuseIdentifier = "Cell"
+  
+  @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      tableView.rowHeight = UITableView.automaticDimension // Dynamic Row Height
 
-        // Do any additional setup after loading the view.
+      let nib = UINib(nibName: "CustomTableViewCell", bundle: nil)
+      self.tableView.register(nib, forCellReuseIdentifier: cellReuseIdentifier)
     }
+  
+  func configureWithPlaces(_ places: [GooglePlace]) {
+    self.placesArray = places
+    if let tableView = tableView {
+      tableView.reloadData()
+    }
+  }
+  
+  
+  @IBAction func doneButton(_ sender: UIBarButtonItem) {
+    dismiss(animated: true)
+  }
+}
+// MARK: - TableView Data Source & TableView Delegate
+extension PlacesViewController: UITableViewDataSource, UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return placesArray.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! CustomTableViewCell
+    let place = placesArray[indexPath.row]
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    cell.nameLabel.text = place.name
+    cell.addressLabel.text = place.address
+    if let rating = place.rating {
+      cell.ratingPLace.text = "Рейтинг \(rating) баллов"
+    } else {
+      cell.ratingPLace.text = ""
     }
-    */
-
+    networkManager.getPlaceIcon(urlImage: place.icon) { (result) in
+      switch result {
+      case .success(let data):
+        cell.iconOfPlace.image = UIImage(data: data)
+      case .failure(let error):
+        print(error)
+      }
+    }
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
 }
